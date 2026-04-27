77804a9901456ebd61e93d74b96679bd99722240
beatrizaraujojs <beatriz20araujo123@gmail.com>
2026-04-27 11:58:20 -0300

    Banco de dados e configurações

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -3,6 +3,10 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
+use App\Models\Categoria;
+
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +23,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+       View::composer('partials.header', function ($view){
+            //buscar todas as categorias ordenar por nome
+            $categorias = Categoria::orderBy('nome_categoria')->get();
+ 
+            dd($categorias);
+ 
+            $view->with('categorias', $categorias);
+        });
     }
 }
